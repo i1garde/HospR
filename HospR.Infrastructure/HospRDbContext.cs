ccb98281602d0a1656d89d6c2a31a05694562729
Ivan Komarenko <vanyakomarenko@gmail.com>
2022-05-03 12:12:11 +0300

    added OnConfiguring method in DbContext + Connections String in appsettings.json

--- HospR.Infrastructure/HospRDbContext.cs
+++ HospR.Infrastructure/HospRDbContext.cs
@@ -4,10 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace HospR.Infrastructure
 {
     internal class HospRDbContext : DbContext
     {
+        public HospRDbContext(DbContextOptions<HospRDbContext> options)
+            : base(options)
+        {
+            
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("AppDb");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
